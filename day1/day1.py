6dce921efb89cb1bd96a39f8e7b36e1859fdc821
Val-Resh <80176765+Val-Resh@users.noreply.github.com>
2021-12-01 14:16:42 +0000

    Update day1.py

--- day1/day1.py
+++ day1/day1.py
@@ -45,10 +45,62 @@ Data in data.txt
 ANSWER: 1766
 """
 
+#FIRST QUESTION
 with open("data.txt", "r") as data:
     count = 0
     data_input = data.readlines()
     for i in range(1, len(data_input)):
         if(int(data_input[i]) > int(data_input[i-1])):
+            count += 1
+    print("Answer: {i}".format(i=count))
+    
+"""--- Part Two ---
+Considering every single measurement isn't as useful as you expected: there's just too much noise in the data.
+
+Instead, consider sums of a three-measurement sliding window. Again considering the above example:
+
+199  A      
+200  A B    
+208  A B C  
+210    B C D
+200  E   C D
+207  E F   D
+240  E F G  
+269    F G H
+260      G H
+263        H
+Start by comparing the first and second three-measurement windows. The measurements in the first window are marked A (199, 200, 208); their sum is 199 + 200 + 208 = 607. The second window is marked B (200, 208, 210); its sum is 618. The sum of measurements in the second window is larger than the sum of the first, so this first comparison increased.
+
+Your goal now is to count the number of times the sum of measurements in this sliding window increases from the previous sum. So, compare A with B, then compare B with C, then C with D, and so on. Stop when there aren't enough measurements left to create a new three-measurement sum.
+
+In the above example, the sum of each three-measurement window is as follows:
+
+A: 607 (N/A - no previous sum)
+B: 618 (increased)
+C: 618 (no change)
+D: 617 (decreased)
+E: 647 (increased)
+F: 716 (increased)
+G: 769 (increased)
+H: 792 (increased)
+In this example, there are 5 sums that are larger than the previous sum.
+
+Consider sums of a three-measurement sliding window. 
+How many sums are larger than the previous sum?
+
+ANSWER: 1797
+
+"""
+
+#SECOND QUESTION
+with open("data.txt", "r") as data:
+    count = 0
+    data_input = data.readlines()
+    three_measures = []
+    for i in range(0, len(data_input) - 2):
+        sum = int(data_input[i]) + int(data_input[i+1]) + int(data_input[i+2])
+        three_measures.append(sum)
+    for i in range(0, len(three_measures) - 1):
+       if(three_measures[i+1] > three_measures[i]):
             count += 1
     print("Answer: {i}".format(i=count))
